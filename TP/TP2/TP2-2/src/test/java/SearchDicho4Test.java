6209efac4d8f5394db3b2d2e7cef265f177f7fbc
alex6683 <alex6683@hotmail.fr>
2017-10-12 20:03:04 +0200

    bmabma

--- TP/TP2/TP2-2/src/test/java/SearchDicho4Test.java
+++ TP/TP2/TP2-2/src/test/java/SearchDicho4Test.java
@@ -8,14 +8,14 @@ import org.junit.Test;
 import org.junit.rules.Timeout;
 
 import main.java.Article;
-import main.java.Magasin;
+import main.java.Shop;
 
 public class SearchDicho4Test {
 	
 	private Article articleValid;
 	private Article articleValid2;
 	private Article articleValid3;
-	private Magasin magasin;
+	private Shop shop;
 	
 	@Rule
 	public Timeout globalTimeout = Timeout.seconds(5);
@@ -25,60 +25,60 @@ public class SearchDicho4Test {
 		articleValid = new Article("Article1", 50, 1);
 		articleValid2 = new Article("Article2", 51, 2);
 		articleValid3 = new Article("Article3", 52, 3);
-		magasin = new Magasin(10);
-		magasin.insererArticle(articleValid);
-		magasin.insererArticle(articleValid2);
-		magasin.insererArticle(articleValid3);
+		shop = new Shop(10);
+		shop.insertArticle(articleValid);
+		shop.insertArticle(articleValid2);
+		shop.insertArticle(articleValid3);
 	}
 
 	@Test
-	public void chercherDicho4ElementInTableTest() {
+	public void searchDicho4ElementInTableTest() {
 		Article articleToFind = new Article("Article3", 52, 3);
-		assertTrue(magasin.chercherDicho4(articleToFind));
+		assertTrue(shop.searchDicho4(articleToFind));
 		assertTrue(articleValid3.isEqual(articleToFind));
 	}
 	
 	@Test
-	public void chercherDicho4ElementNotInTableTest() {
+	public void searchDicho4ElementNotInTableTest() {
 		Article articleToFindFalse = new Article("ToFindFalse", 51.5, 5);
-		assertFalse(magasin.chercherDicho4(articleToFindFalse));
+		assertFalse(shop.searchDicho4(articleToFindFalse));
 		assertFalse(articleValid3.isEqual(articleToFindFalse));
 	}
 
 	@Test
-	public void chercherDicho4ElementWrongName() {
+	public void searchDicho4ElementWrongName() {
 		Article articleToFindWrongName = new Article("WrongName", 52, 3);
-		assertFalse(magasin.chercherDicho4(articleToFindWrongName));
+		assertFalse(shop.searchDicho4(articleToFindWrongName));
 		assertFalse(articleValid3.isEqual(articleToFindWrongName));	
 	}
 	
 	@Test
-	public void chercherDicho4ElementWrongNumber() {
+	public void searchDicho4ElementWrongNumber() {
 		Article articleToFindWrongNumber = new Article("Article3", 52, 5);
-		assertFalse(magasin.chercherDicho4(articleToFindWrongNumber));
+		assertFalse(shop.searchDicho4(articleToFindWrongNumber));
 		assertFalse(articleValid3.isEqual(articleToFindWrongNumber));
 	}
 	
 	@Test
-	public void chercherDicho4ElementWrongPrice() {
+	public void searchDicho4ElementWrongPrice() {
 		Article articleToFindWrongPrice = new Article("Article3", 52.01, 3);
-		assertFalse(magasin.chercherDicho4(articleToFindWrongPrice));
+		assertFalse(shop.searchDicho4(articleToFindWrongPrice));
 		assertFalse(articleValid3.isEqual(articleToFindWrongPrice));
 	}
 	
 	@Test
-	public void chercherDichoElementMini() {
+	public void searchDichoElementMini() {
 		Article articleFree = new Article("Article3", 0.00, 3);
-		assertFalse(magasin.chercherDicho4(articleFree)) ;
-		magasin.insererArticle(articleFree);
-		assertTrue(magasin.chercherDicho4(articleFree)) ;
+		assertFalse(shop.searchDicho4(articleFree)) ;
+		shop.insertArticle(articleFree);
+		assertTrue(shop.searchDicho4(articleFree)) ;
 	}
 	
 	@Test
-	public void chercherDichoElementMax() {
+	public void searchDichoElementMax() {
 		Article articleExpensive = new Article("Article3", 10000.01, 3);
-		assertFalse(magasin.chercherDicho4(articleExpensive)) ;
-		magasin.insererArticle(articleExpensive);
-		assertTrue(magasin.chercherDicho4(articleExpensive)) ;
+		assertFalse(shop.searchDicho4(articleExpensive)) ;
+		shop.insertArticle(articleExpensive);
+		assertTrue(shop.searchDicho4(articleExpensive)) ;
 	}
 }
